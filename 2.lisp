55d8938470a39855f7272328aaac2e5f09e70631
Osteli1998 <60349293+Osteli1998@users.noreply.github.com>
2020-01-27 17:39:02 +0300

    Create 2.lisp

---
+++ 2.lisp
@@ -0,0 +1,7 @@
+;; Чтение файла
+(let ((in (open path :if-does-not-exist nil)))
+    (when in
+        (loop for line = (read-line in nil)
+    while line do (format t "~a~%" line))
+    (close in)))
+(out ("test.txt"))
